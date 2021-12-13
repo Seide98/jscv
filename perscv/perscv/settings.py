43b576ff90c0626226d74275f5f5ef9afcaad5bd
Seide <seide98@hotmail.com>
2021-12-13 12:34:50 +0100

    moved all styleing to separate css files for each kategori

--- perscv/perscv/settings.py
+++ perscv/perscv/settings.py
@@ -122,6 +122,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
+MEDIA_URL = "/documents/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
