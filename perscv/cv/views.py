10bbb42b2f2e5791d7d64bb676929571881c22d9
Seide <seide98@hotmail.com>
2021-12-13 16:11:38 +0100

    Added contact me function and a .env file for security credentials

--- perscv/cv/views.py
+++ perscv/cv/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 import datetime
 from django.utils import timezone
+from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.urls import reverse
 
-# Create your views here.
 
 def index(request):
     now = timezone.now()
     now = datetime.datetime.now().strftime('%H:%M:%S')
     print(now)
-    return render(request, 'index.html')
+
+    #contact me form
+    if request.method == 'POST':
+        personname = request.POST['personname']
+        email = request.POST['email']
+        subject = request.POST['subject']
+
+        #send email
+
+        send_mail(
+            'Message from ' + personname, # subject
+            subject, # message
+            email, # from email
+            ['seidee98@gmail.com'], # to email
+        )
+
+        return render(request, 'index.html', {'personname': personname})
+    else:
+        return render(request, 'index.html', {})
--- perscv/perscv/settings.py
+++ perscv/perscv/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*yzv5v62lg=&@j(1i2=s^wu+*57)xloh)_n%g8*@@wgnf_la)g'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == "1" # 1 == True
 
 ALLOWED_HOSTS = []
 
@@ -99,6 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+#Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 
 # Internationalization
