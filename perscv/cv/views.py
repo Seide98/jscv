6f71903c99dd471a4a05a7c8e93f6bf081f1bf6c
Seide <seide98@hotmail.com>
2022-01-13 18:07:51 +0100

    Added alot of more stuff

--- perscv/cv/views.py
+++ perscv/cv/views.py
@@ -15,20 +15,21 @@ def index(request):
     #contact me form
     if request.method == 'POST':
         personname = request.POST['personname']
-        email = request.POST['email']
+        email = request.POST.get('email')
         subject = request.POST['subject']
-
-        #send email
-
-        send_mail(
-            'Message from ' + personname, # subject
-            subject, # message
-            email, # from email
-            ['seidee98@gmail.com'], # to email
-        )
-        return render(request, 'index.html', {'personname': personname})
+        if email and subject:
+            #send email
+            send_mail(
+                'Message from ' + personname + ' ' + email, # subject
+                subject, # message
+                email, # from email
+                ['seidee98@gmail.com'], # to email
+            )
+            print(email)
+            return render(request, 'index.html', {'personname': personname})
+        else:
+            return render(request, 'index.html', {})
     else:
-
         return render(request, 'index.html', {})
 
 def presentation(request):
